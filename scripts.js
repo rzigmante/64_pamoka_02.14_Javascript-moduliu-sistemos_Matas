18db539a04da29fb263ae2daf32afff898de632e
Raimonda Zigmante <raices7@yahoo.com>
2023-02-14 21:03:05 +0200

    uzdeti riestiniai skliaustai multiply

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@
 
 //Sukurkite modulį (t.y. file'ą - multi.js), kuriame būtų viena eksportuota (default) funkcija - multiplikacija. Ji priims du skaičius kaip parametrus ir grąžins šiuos skaičius sudaugintus. Pagrindiniame JS file'e (script.js) importuokite ir panaudokite šį modulį.
 
-import multiply from "./multi.js";
+import { multiply } from "./multi.js";
 
 const result = multiply(2, 3);
 console.log(result);
